f349165cc1e94f5a7152b1040d193a96b0324d7b
Jeff Santarossa <jeff.santarossa@zalando.de>
2016-02-15 17:30:18 +0100

    Started work on revocations.

--- revoke/caching.go
+++ revoke/caching.go
@@ -1,23 +1,25 @@
 package revoke
 
 import (
+	"github.com/zalando/planb-tokeninfo/options"
 	"time"
 )
 
-const EXPIRE_LENGTH = 8 * 60 * 60 // 8 hours
+// TODO: consider adding a separate cache for each revocation type
 
-// TODO: not sure how to handle GLOBAL, at the moment
 type Cache struct {
 	get    chan *request
 	set    chan *request
 	del    chan *request
 	expire chan bool
+	ts     chan *request
+	cName  chan *request
 }
 
 type request struct {
 	key string
-	val *Revocation
-	res chan *Revocation
+	val interface{}
+	res chan interface{}
 }
 
 func NewCache() *Cache {
@@ -25,10 +27,12 @@ func NewCache() *Cache {
 	get := make(chan *request)
 	set := make(chan *request)
 	del := make(chan *request)
-	expire := make(chan *requst)
+	expire := make(chan bool)
+	ts := make(chan *request)
+	cName := make(chan *request)
 
 	go func() {
-		c := make(map[string]*Revocation)
+		c := make(map[string]interface{})
 
 		for {
 			select {
@@ -36,59 +40,94 @@ func NewCache() *Cache {
 				c[r.key] = r.val
 			case r := <-del:
 				delete(c, r.key)
-			case r := <-expire:
+			case <-expire:
 				for key, revocation := range c {
-					if isExpired(revocation.Timestamp) {
+					if isExpired(revocation.(*Revocation).Timestamp) {
 						delete(c, key)
 					}
 				}
+			case r := <-ts:
+				t := 0
+				key := ""
+				for k, rev := range c {
+					if rev.(*Revocation).Timestamp > t {
+						t = rev.(*Revocation).Timestamp
+						key = k
+					}
+				}
+				r.res <- c[key]
+			case r := <-cName:
+				names := make(map[string]int)
+				for _, rev := range c {
+					if rev.(*Revocation).Type == "CLAIM" {
+						names[rev.(*Revocation).Data["name"].(string)] = 1
+					}
+				}
+				r.res <- names
 			case r := <-get:
-				r.val <= c[r.key]
+				r.res <- c[r.key]
 			}
 		}
 	}()
 
-	return &Cache{get: get, set: set, del: del, expire: expire}
+	return &Cache{get: get, set: set, del: del, expire: expire, ts: ts, cName: cName}
 }
 
-func (c *Cache) Get(key string) *Revocation {
-	res := make(chan *Revocation)
+func (c *Cache) Get(key string) interface{} {
+	res := make(chan interface{})
 	c.get <- &request{key: key, res: res}
 	return <-res
+}
+
+func (c *Cache) GetLastTS() int {
+	res := make(chan interface{})
+	c.ts <- &request{res: res}
+	r := <-res
+	return r.(*Revocation).Timestamp
+}
+
+func (c *Cache) GetClaimNames() []string {
+	res := make(chan interface{})
+	c.cName <- &request{res: res}
+	r := <-res
+	var names []string
+	for n, _ := range r.(map[string]int) {
+		names = append(names, n)
+	}
+
+	return names
 }
 
 func (c *Cache) Expire() {
 	c.expire <- true
 }
 
-func (c *Cache) Add(revoke *Revocation) {
+func (c *Cache) Add(rev *Revocation) {
 	var hash string
-	switch revoke.Type {
+	switch rev.Type {
 	case "TOKEN":
-		hash = revoke.Data["token_hash"]
+		hash = rev.Data["token_hash"].(string)
 	case "CLAIM":
-		hash = revoke.Data["value_hash"]
+		hash = rev.Data["name"].(string) + rev.Data["value_hash"].(string)
 	case "GLOBAL":
-	// TODO
+		hash = "GLOBAL"
 	default:
 		return
 	}
-	c.set <- &request{key: hash, value: revoke}
+	c.set <- &request{key: hash, val: rev}
 }
 
 func (c *Cache) Delete(key string) {
 	c.del <- &request{key: key}
 }
 
-func isExpired(string ts) bool {
-	t, err := strconv.Atoi(ts)
-	if err != null {
-		log.Errorf("Error converting timestamp to int. " + err.Error())
-	}
+func isExpired(ts int) bool {
 
-	if t-EXPIRE_LENGTH < time.Now().Unix() {
+	if ts-int(options.RevokeExpireLength) < int(time.Now().UnixNano()/1e6) {
 		return true
 	}
 
 	return false
 }
+
+// vim: ts=4 sw=4 noexpandtab nolist syn=go
--- revoke/revoke.go
+++ revoke/revoke.go
@@ -2,8 +2,7 @@ package revoke
 
 import (
 	"encoding/json"
-	"errors"
-	"strconv"
+	"log"
 	"time"
 )
 
@@ -17,93 +16,91 @@ type jsonRevoke struct {
 	Meta struct {
 		ForceRefresh string `json:"force_refresh"`
 	} `json:"meta"`
-	Revocation []struct {
-		Type      string `json:"type"` // TOKEN, CLAIM, GLOBAL
-		RevokedAt string `json:"revoked_at"`
-		Data      struct {
-			Name         string `json:"name,omitempty"`          // CLAIM
-			ValueHash    string `json:"value_hash,omitempty"`    // CLAIM
-			IssuedBefore string `json:"issued_before,omitempty"` // CLAIM, GLOBAL
-			TokenHash    string `json:"token_hash,omitempty"`    // TOKEN
-			RevokedAt    string `json:"revoked_at,omitempty"`    // TOKEN
-		} `json:"data"`
-	} `json:"revocations"`
+	Revs []jsonRevocation `json:"revocations"`
 }
 
-func (r *jsonRevoke) UnmarshallJSON(data []byte, forcedRefresh bool) (err error) {
-	var buf jsonRevoke
-	if err = json.Unmarshall(data, &buf); err != nil {
-		log.Errorf("Error unmarshalling revocation json. " + err.Error())
+type jsonRevocation struct {
+	Type      string `json:"type"` // TOKEN, CLAIM, GLOBAL
+	RevokedAt int    `json:"revoked_at"`
+	Data      struct {
+		Name          string `json:"name,omitempty"`           // CLAIM
+		ValueHash     string `json:"value_hash,omitempty"`     // CLAIM
+		IssuedBefore  int    `json:"issued_before,omitempty"`  // CLAIM, GLOBAL
+		TokenHash     string `json:"token_hash,omitempty"`     // TOKEN
+		HashAlgorithm string `json:"hash_algorithm,omitempty"` // CLAIM, TOKEN
+	} `json:"data"`
+}
+
+func (r *jsonRevoke) UnmarshallJSON(data []byte) (err error) {
+	if err = json.Unmarshal(data, &r); err != nil {
+		log.Println("Error unmarshalling revocation json. " + err.Error())
 		return err
 	}
 
 	// Note: if we already foreced a refresh, we don't want to do it again
 	// otherwise we'll get stuck in an infinite loop
-	if buf.ForceRefresh != "" && !forcedRefresh {
-		i, err := strconv.Atoi(buf.ForceRefresh)
-		if err != nil {
-			log.Errorf("Error converting ForceRefresh to int." + err.Error())
-		} else {
-			// TODO: not sure how to get the current Cache from here. . .
-			refreshCacheFromTime(i)
-		}
-	}
-
-	return
-}
-
-func (r []*Revocation) getRevocationFromJson(json *jsonRevoke.Revocation) {
-
-	t := int32(time.Now().Unix())
-	for j := range json {
-
-		switch j.Type {
-		case "TOKEN":
-			valid := isHashTimestampValid(j.Data.TokenHash, j.Data.RevokedAt)
-			if !valid {
-				log.Errorf("Invalid revocation data. TokenHash: %s, RevokedAt: %s", j.Data.TokenHash, j.Data.RevokedAt)
-				continue
-			}
-			r.Data["token_hash"] = j.Data.TokenHash
-			r.Data["revoked_at"] = j.Data.RevokedAt
-		case "CLAIM":
-			valid := isHashTimestampValid(j.Data.ValueHash, j.Data.IssuedBefore)
-			if !valid {
-				log.Errorf("Invalid revocation data. ValueHash: %s, IssuedBefore: %s", j.Data.ValueHash, j.Data.IssuedBefore)
-				continue
-			}
-			r.Data["value_hash"] = j.Data.ValueHash
-			r.Data["issued_before"] = j.Data.IssuedBefore
-			r.Data["name"] = j.Data.Name
-		case "GLOBAL":
-			_, err := strconv.Atoi(j.Data.IssuedBefore)
+	/*
+		if buf.ForceRefresh != "" && !forcedRefresh {
+			i, err := strconv.Atoi(buf.ForceRefresh)
 			if err != nil {
-				log.Errorf("Erorr converting IssuedBefore to int. " + err.Error())
-				continue
+				log.Println("Error converting ForceRefresh to int." + err.Error())
+			} else {
+				// TODO: not sure how to get the current Cache from here. . .
+				refreshCacheFromTime(i)
 			}
-			r.Data["issued_before"] = j.Data.IssuedBefore
-		default:
-			log.Errorf("Unsupported revocation type: %s", j.Type)
-			continue
 		}
+	*/
 
-		r.Type = j.Type
-		r.Timestamp = t
-	}
 	return
 }
 
-func isHashTimestampValid(hash, timestamp string) bool {
-	if hash == "" || timestamp == "" {
-		return false
+func (r *Revocation) getRevocationFromJson(j *jsonRevocation) {
+
+	// account for some network delay, say three seconds
+	t := int(time.Now().UnixNano()/1e6) - 3*1e6
+
+	r.Data = make(map[string]interface{})
+	switch j.Type {
+	case "TOKEN":
+		valid := isHashTimestampValid(j.Data.TokenHash, j.RevokedAt)
+		if !valid {
+			log.Println("Invalid revocation data (TOKEN). TokenHash: %s, RevokedAt: %d", j.Data.TokenHash, j.RevokedAt)
+			return
+		}
+		r.Data["token_hash"] = j.Data.TokenHash
+		r.Data["revoked_at"] = j.RevokedAt
+	case "CLAIM":
+		valid := isHashTimestampValid(j.Data.ValueHash, j.Data.IssuedBefore)
+		if !valid {
+			log.Println("Invalid revocation data (CLAIM). ValueHash: %s, IssuedBefore: %d", j.Data.ValueHash, j.Data.IssuedBefore)
+			return
+		}
+		if j.Data.Name == "" {
+			log.Println("Invalid revocation data (missing claim name).")
+			return
+		}
+		r.Data["value_hash"] = j.Data.ValueHash
+		r.Data["issued_before"] = j.Data.IssuedBefore
+		r.Data["name"] = j.Data.Name
+	case "GLOBAL":
+		valid := isHashTimestampValid("thisStringDoesntMatter", j.Data.IssuedBefore)
+		if !valid {
+			log.Println("Invalid revocation data (GLOBAL). IssuedBefore: %d", j.Data.IssuedBefore)
+			return
+		}
+		r.Data["issued_before"] = j.Data.IssuedBefore
+	default:
+		log.Println("Unsupported revocation type: %s", j.Type)
+		return
 	}
 
-	_, err := strconv.Atoi(timestamp)
-	if err != nil {
-		log.Errorf("Erorr converting timestamp to int. " + err.Error())
-		return false
-	}
-
-	return true
-
+	r.Type = j.Type
+	r.Timestamp = t
+	return
 }
+
+func isHashTimestampValid(hash string, timestamp int) bool {
+	return hash != "" && timestamp != 0
+}
+
+// vim: ts=4 sw=4 noexpandtab nolist syn=go
